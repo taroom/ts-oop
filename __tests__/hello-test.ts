b2d62e4ff76ee09971ccbe36434545efdb9866c0
Sultan Room <sultanroomcode@gmail.com>
2024-06-05 14:05:52 +0700

    add jest test

---
+++ __tests__/hello-test.ts
@@ -0,0 +1,6 @@
+describe('Hello', function() {
+    it("Seharusnya berkata hello", function(){
+        const name = 'Hallo Agus Sutarom';
+        expect(name).toBe('Hallo Agus Sutarom');
+    })
+})
