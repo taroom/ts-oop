e47d849a8d8b931a0a3b9a9818280b047d3716d8
Sultan Room <sultanroomcode@gmail.com>
2024-06-09 16:56:27 +0700

    add include option

--- tests/hello-test.ts
+++
@@ -1,6 +0,0 @@
-describe('Hello', function() {
-    it("Seharusnya berkata hello", function(){
-        const name = 'Hallo Agus Sutarom';
-        expect(name).toBe('Hallo Agus Sutarom');
-    })
-})
